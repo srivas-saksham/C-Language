a95cecd930c90b695078aa3c6a80c7a6f6821499
Saksham S <sakshamsrivastava034060@gmail.com>
2024-10-07 13:17:44 +0530

    Lab Ques 1-2

---
+++ Lab Assignments/Lab File Programs/Question 1/main.c
@@ -0,0 +1,12 @@
+// WAP to demonstrate the usage of escape sequences (\n, \\, \t, \b, \", \')
+#include<stdio.h>
+
+int main(){
+    printf("Program to demonstrate the usage of escape sequences: \n");
+    printf("This statement started from the next line as a newline character (\\n) is used at the end of previous print statement.\n");
+    printf("In previous line the Backslash Escape character (\\\\) is used. \n");
+    printf("\t<--- Here 4 Tab Spaces are given using Horizontal Tab (\\t). \n");
+    printf("Backspace Character (\\b) to move cursor  \bone character back.\n");
+    printf("Using Double Quote (\\\") \"To print the Double Quotes\". \n");
+    printf("Using Single Quote (\\\') \'To print the Single Quotes\'. \n");
+}
---
+++ Lab Assignments/Lab File Programs/Question 2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // Declaring and initializing integer variables
+    int intVar1 = 10;
+    int intVar2 = 20;
+
+    // Declaring and initializing float variables
+    float floatVar1 = 12.5;
+    float floatVar2 = 30.75;
+
+    // Declaring and initializing constants using 'const' keyword
+    const int intConst = 100;    // Integer constant i.e cannot be updated
+    const float floatConst = 50.25; // Float constant i.e cannot be updated
+
+    // Printing values of integer variables
+    printf("Integer Variable 1: %d\n", intVar1);
+    printf("Integer Variable 2: %d\n", intVar2);
+
+    // Printing values of float variables
+    printf("Float Variable 1: %.2f\n", floatVar1);  // .2f to print 2 decimal places
+    printf("Float Variable 2: %.2f\n", floatVar2);
+
+    // Printing values of integer and float constants
+    printf("Integer Constant: %d\n", intConst);
+    printf("Float Constant: %.2f\n", floatConst);
+
+    return 0;
+}
