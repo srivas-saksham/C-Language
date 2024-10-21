6f0ae4825bb2295b77e4467c735e4946bb2cada3
Saksham S. <sakshamsrivastava034060@gmail.com>
2024-10-21 12:03:30 +0530

    Question 44

---
+++ Lab Assignments/Lab File Programs/Question 44/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int maximum(int n, int a[]){
+    int max = a[0];
+
+    for (int i = 1; i < n; i++){
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+}
+int minimum(int n, int a[]){
+    int min = a[0];
+
+    for (int i = 1; i < n; i++){
+        if (a[i] < min) {
+            min = a[i];
+        }
+    }
+    return min;
+}
+int main() {
+    int n,max,min;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    max = maximum(n, arr);
+    min = minimum(n, arr);
+    printf("Max Number in Array is %d\n", max);
+    printf("Max Number in Array is %d\n", min);
+
+    return 0;
+}
